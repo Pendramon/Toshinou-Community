82cce6c6b96d5f84eece71a64caaf3f6d0b02d37
Pendramon <Pendramon2@gmail.com>
2018-07-01 01:11:28 +0200

    v1.0.5: On ship select/destroyed update attack details.

--- src/js/handlers/commands/ShipRemovedHandler.js
+++ src/js/handlers/commands/ShipRemovedHandler.js
@@ -12,18 +12,15 @@ class ShipRemovedHandler {
       var parsed = JSON.parse(e.detail);
       var id = parsed.userId;
 
-      if (a.targetShip && id == a.targetShip.id) {
+      if (a.targetShip && id == a.targetShip.id || a.lockedShip && id == a.lockedShip.id) {
         a.targetShip = null;
         a.attacking = false;
         a.triedToLock = false;
         a.lockedShip = null;
         a.lastAutoLock = null;
-      } else if (a.lockedShip && id == a.lockedShip.id) {
-        a.targetShip = null;
-        a.attacking = false;
-        a.triedToLock = false;
-        a.lockedShip = null;
-        a.lastAutoLock = null;
+        window.attackWindow.hp("0");
+        window.attackWindow.shd("0");
+        window.attackWindow.targetName("None");
       }
 
       var ship = a.ships[id];

d4c0f4cc9fbdaad6d33f6677ea11819c6e04cc7a
Pendramon <Pendramon2@gmail.com>
2018-07-01 03:36:59 +0200

    v1.0.6: Added a different handler for Ship Destroyed since it is gone.

--- src/js/handlers/commands/ShipDestroyedHandler.js
+++ src/js/handlers/commands/ShipDestroyedHandler.js
@@ -4,26 +4,16 @@ Created by Freshek on 24.10.2017
 
 class ShipDestroyedHandler {
   static get ID() {
-    return 21281;
+    return 10317;
   }
 
   constructor() {
     this._handler = function (e, a) {
-      var parsed = JSON.parse(e.detail);
-      var id = parsed[Variables.shipDestoyedId];
-
-      if (a.targetShip && id == a.targetShip.id) {
-        a.targetShip = null;
-        a.attacking = false;
-        a.triedToLock = false;
-        a.lockedShip = null;
-      }
-
-      var ship = a.ships[id];
-
-      if (ship != null) {
-        delete a.ships[id];
-      }
+      delete a.ships[a.targetShip.id]
+      a.targetShip = null;
+      a.attacking = false;
+      a.triedToLock = false;
+      a.lockedShip = null;
     }
   }
 
